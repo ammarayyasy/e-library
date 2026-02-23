40e7ecb690ec9175b087d86aa4f60217d627693c
ammarayyasy <ammarmojur@gmail.com>
2026-02-23 10:13:04 +0700

    Controller

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -11,6 +11,10 @@ class Book extends Model
 
     protected $guarded = ['id'];
 
+    protected $casts = [
+        'published_at' => 'datetime',
+    ];
+
     public function category()
     {
         return $this->belongsTo(Category::class);
